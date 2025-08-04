a09548e0132315bf5de478028ab06135cee0ec1a
Luis <luis.guaman@fullsegura.com>
2025-08-04 11:22:50 -0500

    video path

--- vlm_alerts/start_mining.sh
+++ vlm_alerts/start_mining.sh
@@ -2,4 +2,4 @@
 cd /home/fullzecure/dev/google/gemma3n/vlm_alerts
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate g1
-python3 mainKagle.py --model_url http://localhost:8000/describe --video_file $HOME/dev/mining3.mp4 --api_key "No_Needed" --loop_video --hide_query
+python3 mainKagle.py --model_url http://localhost:8000/describe --video_file mining3.mp4 --api_key "No_Needed" --loop_video --hide_query
